8c7199d40b6751c3c6a87cb52518dced8c4e7981
Marcus Herrera <maherreraiv1987@gmail.com>
2022-11-14 10:30:52 -0700

    Post repository

---
+++ src/main/java/com/technews/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package com.technews.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.technews.model.Post;
+
+import java.util.List;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Integer> {
+    List<Post> findPostByUserId(Integer id) throws Exception;
+}
--- src/main/java/com/technews/repository/UserRepository.java
+++ src/main/java/com/technews/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.technews.repository;
 
-public interface UserRepository {
+import com.technews.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findUserByEmail(String email) throws Exception;
 }
